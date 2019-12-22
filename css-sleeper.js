5394aead3fd5357f65a4ec08a41b61c0a0edb4b2
Cfouria <cat.alucardo@gmail.com>
2019-12-22 00:40:41 -0600

    how the fuck do i use git

--- css-sleeper.js
+++ css-sleeper.js
@@ -1,19 +1,22 @@
-var styles, tol, tollen, stylelen, current, buttonEleC, buttonEleS
+var styles, tol, tollen, stylelen, current, buttonEleC, buttonEleS, buttonEleN, inlinestyles, fcount
 
 window.addEventListener('load', (event) => {holyshitonloadeventandsimiliarisbroken()});
 (document.readyState === "complete") ? holyshitonloadeventandsimiliarisbroken() : false
 
 window.addEventListener("keydown", function(event) {
-  if (event.target === buttonEleC || event.target === buttonEleS){  
-    if (event.shiftKey  &&  event.code === "KeyC"){
-      shiftC()
-    }
+  if (event.target === buttonEleC || event.target === buttonEleS || event.target === buttonEleN){  
     if (event.shiftKey  &&  event.code === "KeyS"){
       shiftS()
     }
+    if (event.shiftKey  &&  event.code === "KeyC"){
+      shiftC()
+    }
+    if (event.shiftKey  &&  event.code === "KeyN"){
+      shiftN()
+    }
   }
   if (event.altKey  &&  event.code === "KeyC"){
-    console.log('alt+c not in use? well sites like youtube block the top soooo...');
+    console.log('sites like youtube block the buttons soooo...alt+c not in use');
     shiftC()
   }
 }, true);
@@ -28,36 +31,64 @@ function holyshitonloadeventandsimiliarisbroken(){
   styles = [...document.styleSheets]
 //  styles.forEach(e=>e.disabled=true) //disable on load
   tol = splitter(styles,3)
-  tol.unshift(styles)
   tol.unshift([])
+  tol.unshift(styles)
   tollen = tol.length
   stylelen = styles.length
   current = 0
   console.log(tol);
   buttons()
+  fcount = []
+  shiftLoad()
 }
 
-function shiftC(){
-  styles.forEach(e=>e.disabled=false)
-  tol[(current++ % tollen)].forEach(e=>e.disabled=true)
-}
 function shiftS(){
   styles[(current % stylelen)].disabled = !styles[(current % stylelen)].disabled
   current++
+  fcount.push('s')
+}
+function shiftC(){
+  styles.forEach(e=>e.disabled=false)
+  tol[(current++ % tollen)].forEach(e=>e.disabled=true)
+  fcount.push('c')
+}
+function shiftN(){
+  if (document.querySelectorAll('[style]').length) {true} else {inlinestyles.length ? inlinestyles.map(e=>{e.remove();inlinestyles=[]}):false}
+  inlinestyles= [...document.querySelectorAll('[style]')]
+  inlinestyles.map(e=>e.removeAttribute('style'))
+  fcount.push('n')
+}
+function shiftL(){
+  localStorage.setItem('cssSleeper', JSON.stringify(fcount));
+}
+function shiftLoad(){
+  JSON.parse(localStorage.getItem('cssSleeper')).forEach(e=>{e==='s'?shiftS():false;e==='c'?shiftC():false;e==='n'?shiftN():false;})
 }
 
 function buttons(){
   buttonEleC = document.createElement('button');
   buttonEleS = document.createElement('button');
+  buttonEleN = document.createElement('button');
+  buttonEleL = document.createElement('button');
   buttonEleC.type = 'button';
   buttonEleS.type = 'button';
+  buttonEleN.type = 'button';
+  buttonEleL.type = 'button';
   buttonEleC.title = 'otherwise it will trigger while typing';
   buttonEleS.title = 'button needs to be focus for keypress';
+  buttonEleN.title = '1st kill inline styles & 2nd purge styled elements';
+  buttonEleL.title = 'Keeps button click count in localstorage to executes css-sleeper buttons: CSN on page load. Note! Dont go crazy with buttons.';
   buttonEleC.textContent = "css-sleeper shift+C"
   buttonEleS.textContent = "css-sleeper shift+S"
+  buttonEleN.textContent = "css-sleeper shift+N"
+  buttonEleL.textContent = "css-sleeper Keep"
+  document.body.prepend(buttonEleL)
+  document.body.prepend(buttonEleN)
   document.body.prepend(buttonEleC)
   document.body.prepend(buttonEleS)
   buttonEleC.onclick = shiftC
   buttonEleS.onclick = shiftS
+  buttonEleN.onclick = shiftN
+  buttonEleN.onclick = shiftL
 }
 
